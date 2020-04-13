f7358aeae6b98d4d87196c12c7027505667dd385
cc3069 <61521909+cc3069@users.noreply.github.com>
2020-04-13 14:16:50 -0400

    buttons commit

--- sketch.js
+++ sketch.js
@@ -1,3 +1,36 @@
+let input, button, greeting;
+
+function setup() {
+  // create canvas
+  createCanvas(windowWidth,windowHeight);
+
+  input = createInput();
+  input.position(windowWidth, windowHeight);
+
+  // appear after certain amt of time: button = createButton('submit');
+  button.position(input.x + input.width, 10);
+  button.mousePressed(greet);
+  greeting = createElement('h2', 'Memory Art Game');
+  greeting.position(10, 1);
+
+  textAlign(CENTER);
+  textSize(30);
+}
+
+function greet() {
+  const name = input.value();
+  greeting.html('hello ' + name + '!');
+  input.value('');
+  
+  for (let i = 0; i < 200; i++) {
+    push();
+    fill(random(0), 10, 100);
+    translate(random(width), random(height));
+    rotate(random(2 * PI));
+    text(name, 0, 0);
+    pop();
+  }
+}
 //let button;
 let timer1=30;
 let timer2;
